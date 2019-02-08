e860ddf7c044427209b4c658dac4bd34d78e55c7
Ryan <ryan.david.pascal@gmail.com>
2019-02-07 21:31:36 -0500

    End part 1

--- project_1/bigInt435/rsa435.cc
+++ project_1/bigInt435/rsa435.cc
@@ -4,9 +4,71 @@
 #include <string>
 #include <iostream>
 #include <stdlib.h>
+#include <fstream>
 
 // 'BigIntegerLibrary.hh' includes all of the library headers.
 #include "BigIntegerLibrary.hh"
+
+bool isRelativelyPrime(BigUnsigned x, BigUnsigned y);
+BigUnsigned getRandomNumber(BigUnsigned k);
+BigUnsigned modExp(BigUnsigned x, BigUnsigned y, BigUnsigned m);
+bool fermatTest(BigUnsigned N);
+BigUnsigned getLargeRandomPrime();
+BigUnsigned getE(BigUnsigned theta);
+
+int main()
+{
+   /* The library throws 'const char *' error messages when things go
+	 * wrong.  It's a good idea to catch them using a 'try' block like this
+	 * one.  Your C++ compiler might need a command-line option to compile
+	 * code that uses exceptions. */
+   try
+   {
+
+      std::cout << "a couple of test cases for 3460:435/535 Algorithms!!!\n";
+      BigUnsigned p = getLargeRandomPrime();
+      std::cout << p << std::endl;
+      BigUnsigned q = getLargeRandomPrime();
+      std::cout << q << std::endl;
+
+      BigUnsigned n = p * q;
+
+      BigUnsigned theta = (p - 1) * (q - 1);
+      if (theta % 2 == 0)
+      {
+         theta++;
+      }
+
+      BigUnsigned e = getE(theta);
+      std::cout << e << std::endl;
+
+      BigUnsigned d = modinv(e, theta);
+      std::cout << d << std::endl;
+
+      std::ofstream p_q("p_q.txt");
+      p_q << p << std::endl;
+      p_q << q << std::endl;
+      p_q.close();
+
+      std::ofstream e_n("e_n.txt");
+      e_n << e << std::endl;
+      e_n << n << std::endl;
+      e_n.close();
+
+      std::ofstream d_n("d_n.txt");
+      d_n << d << std::endl;
+      d_n << n << std::endl;
+      d_n.close();
+   }
+   catch (char const *err)
+   {
+      std::cout << "The library threw an exception:\n"
+                << err << std::endl;
+   }
+
+   return 0;
+}
+
 
 bool isRelativelyPrime(BigUnsigned x, BigUnsigned y)
 {
@@ -60,13 +122,8 @@ BigUnsigned getLargeRandomPrime()
    return x;
 }
 
-BigUnsigned getE(BigUnsigned p, BigUnsigned q)
+BigUnsigned getE(BigUnsigned theta)
 {
-   BigUnsigned theta = (p - 1) * (q - 1);
-   if (theta % 2 == 0)
-   {
-      theta++;
-   }
 
    BigUnsigned e = getRandomNumber(20);
    while (!isRelativelyPrime(e, theta))
@@ -74,31 +131,4 @@ BigUnsigned getE(BigUnsigned p, BigUnsigned q)
       e = getRandomNumber(20);
    }
    return e;
-}
-
-int main()
-{
-   /* The library throws 'const char *' error messages when things go
-	 * wrong.  It's a good idea to catch them using a 'try' block like this
-	 * one.  Your C++ compiler might need a command-line option to compile
-	 * code that uses exceptions. */
-   try
-   {
-
-      std::cout << "a couple of test cases for 3460:435/535 Algorithms!!!\n";
-      BigUnsigned p = getLargeRandomPrime();
-      std::cout << p << std::endl;
-      BigUnsigned q = getLargeRandomPrime();
-      std::cout << q << std::endl;
-
-      BigUnsigned e = getE(p, q);
-      std::cout << e << std::endl;
-   }
-   catch (char const *err)
-   {
-      std::cout << "The library threw an exception:\n"
-                << err << std::endl;
-   }
-
-   return 0;
 }
