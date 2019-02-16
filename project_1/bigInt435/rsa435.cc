7266c57aab8e9d151eeb40106abec2b22eda9f09
Ryan <ryan.david.pascal@gmail.com>
2019-02-16 12:14:03 -0500

    almost done with project

--- project_1/bigInt435/rsa435.cc
+++ project_1/bigInt435/rsa435.cc
@@ -5,27 +5,28 @@
 #include <iostream>
 #include <stdlib.h>
 #include <fstream>
+#include <cstdlib>
+#include <ctime>
 
 // 'BigIntegerLibrary.hh' includes all of the library headers.
 #include "BigIntegerLibrary.hh"
 
 bool isRelativelyPrime(BigUnsigned x, BigUnsigned y);
 BigUnsigned getRandomNumber(BigUnsigned k);
-BigUnsigned modExp(BigUnsigned x, BigUnsigned y, BigUnsigned m);
 bool fermatTest(BigUnsigned N);
 BigUnsigned getLargeRandomPrime();
 BigUnsigned getE(BigUnsigned theta);
 
 int main()
 {
+   srand(time(NULL));
+
    /* The library throws 'const char *' error messages when things go
 	 * wrong.  It's a good idea to catch them using a 'try' block like this
 	 * one.  Your C++ compiler might need a command-line option to compile
 	 * code that uses exceptions. */
    try
    {
-
-      std::cout << "a couple of test cases for 3460:435/535 Algorithms!!!\n";
       BigUnsigned p = getLargeRandomPrime();
       std::cout << p << std::endl;
       BigUnsigned q = getLargeRandomPrime();
@@ -69,7 +70,6 @@ int main()
    return 0;
 }
 
-
 bool isRelativelyPrime(BigUnsigned x, BigUnsigned y)
 {
    return gcd(x, y) == 1;
@@ -85,31 +85,11 @@ BigUnsigned getRandomNumber(BigUnsigned k)
    return x;
 }
 
-BigUnsigned modExp(BigUnsigned x, BigUnsigned y, BigUnsigned m)
-{
-   if (y == 0)
-   {
-      return BigUnsigned(1);
-   }
-   else
-   {
-      BigUnsigned z = modExp(x, y / 2, m);
-      if (y % 2 == 0)
-      {
-         return (z * z % m);
-      }
-      else
-      {
-         return (x * z * z % m);
-      }
-   }
-}
-
 bool fermatTest(BigUnsigned N)
 {
    BigUnsigned a1(1);
    BigUnsigned a2(2);
-   return (modExp(a1, N - 1, N) == 1) && (modExp(a2, N - 1, N) == 1);
+   return (modexp(a1, N - 1, N) == 1) && (modexp(a2, N - 1, N) == 1);
 }
 
 BigUnsigned getLargeRandomPrime()
@@ -125,10 +105,10 @@ BigUnsigned getLargeRandomPrime()
 BigUnsigned getE(BigUnsigned theta)
 {
 
-   BigUnsigned e = getRandomNumber(20);
+   BigUnsigned e = getRandomNumber(100);
    while (!isRelativelyPrime(e, theta))
    {
-      e = getRandomNumber(20);
+      e = getRandomNumber(100);
    }
    return e;
 }
--- project_1/messageDigest435/messageDigest435.cpp
+++ project_1/messageDigest435/messageDigest435.cpp
@@ -8,60 +8,100 @@
 #include "sha256.h"
 #include "BigIntegerLibrary.hh"
 
- 
+void getTwoLinesFromFile(std::string file, BigUnsigned &d, BigUnsigned &n)
+{
+   std::ifstream d_n(file);
+   if (d_n.good())
+   {
+      std::string D;
+      std::string N;
+      std::getline(d_n, D);
+
+      std::getline(d_n, N);
+      d = stringToBigUnsigned(D);
+      n = stringToBigUnsigned(N);
+   }
+}
+
+void print(std::string title, BigUnsigned data)
+{
+   std::cout << title << ": " << data << std::endl
+             << std::endl;
+}
+
 int main(int argc, char *argv[])
 {
-   //demonstrating how sha256 works
-   std::string input = "testing";
-   std::string output1 = sha256(input);
-   std::cout << "sha256('"<< input << "'):" << output1 << "\n";
-   
-   //demo bigInt works here
-   BigUnsigned a = stringToBigUnsigned("124338907642982722929222542626327282");
-   BigUnsigned b = stringToBigUnsigned("124338907642977775546469426263278643");
-   std::cout << "big a = " <<a<<"\n";
-   std::cout << "big b = " <<b<<"\n";
-   std::cout << "big a*b = " <<a*b<<"\n";
-
    //Second part of your project starts here
-   if (argc != 3 || (argv[1][0]!='s' && argv[1][0]!='v')) 
+   if (argc != 3 || (argv[1][0] != 's' && argv[1][0] != 'v'))
       std::cout << "wrong format! should be \"a.exe s filename\"";
-   else {
+   else
+   {
       std::string filename = argv[2];
-      
-            
+
       //read the file
-      std::streampos begin,end;
-      std::ifstream myfile (filename.c_str(), std::ios::binary);
+      std::streampos begin, end;
+      std::ifstream myfile(filename.c_str(), std::ios::binary);
       begin = myfile.tellg();
-      myfile.seekg (0, std::ios::end);
+      myfile.seekg(0, std::ios::end);
       end = myfile.tellg();
-      std::streampos size = end-begin;
+      std::streampos size = end - begin;
       //std::cout << "size of the file: " << size << " bytes.\n"; //size of the file
-      
-      myfile.seekg (0, std::ios::beg);
-      char * memblock = new char[size];
-      myfile.read (memblock, size); //read file; it's saved in the char array memblock
+
+      myfile.seekg(0, std::ios::beg);
+      char *memblock = new char[size];
+      myfile.read(memblock, size); //read file; it's saved in the char array memblock
       myfile.close();
-      
-      std::string copyOFfile = filename+".Copy"; 
-      std::ofstream myfile2 (copyOFfile.c_str(), std::ios::binary);
-      myfile2.write (memblock, size); //write to a file
-      myfile2.close();
-      
-      std::cout<<memblock;
-        
-      if (argv[1][0]=='s') {
-         std::cout << "\n"<<"Need to sign the doc.\n";
-         //.....
-         
+
+      // std::string copyOFfile = filename + ".Copy";
+      // std::ofstream myfile2(copyOFfile.c_str(), std::ios::binary);
+      // myfile2.write(memblock, size); //write to a file
+      // myfile2.close();
+
+      std::cout << "memblock: " << memblock << std::endl;
+      std::string memblock_sha = sha256(memblock);
+      std::cout << "memblock_sha: " << memblock_sha << std::endl;
+
+      BigUnsigned mesAsUnsigned = BigUnsigned(BigUnsignedInABase(memblock_sha, 32));
+      print("mesAsUnsigned", mesAsUnsigned);
+
+      if (argv[1][0] == 's')
+      {
+
+         BigUnsigned d;
+         BigUnsigned n;
+         getTwoLinesFromFile("d_n.txt", d, n);
+
+         BigUnsigned sig = modexp(mesAsUnsigned, d, n);
+         print("sig", sig);
+
+         std::ofstream sigOFfile(filename + ".signature");
+         sigOFfile << bigUnsignedToString(sig) << std::endl;
+         sigOFfile.close();
       }
-      else {
-         std::cout << "\n"<<"Need to verify the doc.\n";
-         //.....
-         
+      else
+      {
+         BigUnsigned e;
+         BigUnsigned n;
+         getTwoLinesFromFile("e_n.txt", e, n);
+         BigUnsigned sig;
+
+         std::ifstream sigFile(filename + ".signature");
+         if (sigFile.good())
+         {
+            std::string sigStr;
+            std::getline(sigFile, sigStr);
+            sig = stringToBigUnsigned(sigStr);
+         }
+
+         print("sig", sig);
+
+         BigUnsigned res = modexp(sig, e, n);
+
+         print("res", res);
+
+         std::cout << "Finally: " << (res == mesAsUnsigned) << std::endl;
       }
       delete[] memblock;
-    }
-    return 0;
+   }
+   return 0;
 }
